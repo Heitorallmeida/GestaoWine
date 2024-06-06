6fb1abbfe5a661b7aa1d78ed24c717f452a028cb
GuilhermeAlencarM <futurodevgui@gmail.com>
2024-06-06 17:49:25 -0300

    chore: Update backend dependencies and add client forecast routes

--- backend/routes/detector.py
+++ backend/routes/detector.py
@@ -16,7 +16,7 @@ async def predict(request: Request):
     try:
         model_fit = joblib.load('./detector-model/modelo_previsao.joblib')
         forecast = model_fit.predict(request.numberofdays)
-        return {"prediction": forecast}
+        return {"prediction": forecast.round(2)}
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
     
@@ -49,5 +49,44 @@ async def accuracy():
         mape_value = 0.0216
         return {"mape_accuracy": mape_value,
                 "accuracy": "98%"}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
+
+
+@detector.post("/predict/forecast/client")
+async def predict(request: Request):
+    try:
+        model_fit = joblib.load('./detector-model/modelo_forecast.joblib')
+        forecast = model_fit.predict(request.numberofdays)
+        return {"prediction": forecast.round(2)}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
+    
+@detector.post("/predict/forecast/by-month/client")
+async def predict(request: Request):
+    try:
+        if request.numberofdays != 120:
+            return JSONResponse(status_code=400, content={"error": "O parâmetro 'numberofdays' deve ser igual a 120."})
+
+        model_fit = joblib.load('./detector-model/modelo_forecast.joblib')
+        forecast = model_fit.predict(request.numberofdays)
+ 
+        monthly_forecast = []
+        for i in range(0, len(forecast), 30):  
+            month_forecast = forecast[i:i+30]  
+            monthly_total = np.sum(month_forecast)  #
+            monthly_forecast.append(monthly_total)
+
+        return {"monthly_forecast": monthly_forecast}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
+    
+
+@detector.get("/accuracy/client")
+async def accuracy():
+    try:
+        mape_value = 0.0578
+        return {"mape_accuracy": mape_value,
+                "accuracy": "94%"}
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
